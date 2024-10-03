990124c510580666b03c34486399cb03d015f485
Richard <richardzou25@gmail.com>
2024-10-03 19:42:26 -0400

    completed exercises 1,2, and 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+    let modifiedString = string.split("");
+    let newString = modifiedString.reverse();
+    return newString.join("");
 };
 
 // Do not edit below this line
