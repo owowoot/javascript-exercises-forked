1c4c9af9952e334145e4296fd31d4fde275f5d01
Richard <richardzou25@gmail.com>
2024-10-05 08:58:21 -0400

    completed exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...args) {
+    let newArray = [...array]
+    for (let arg of args) {
+        let index = newArray.indexOf(arg)
+        while (index !== -1) {
+            newArray.splice(index, 1)
+            index = newArray.indexOf(arg)
+        }
+    };
+    return newArray;
 };
 
 // Do not edit below this line
