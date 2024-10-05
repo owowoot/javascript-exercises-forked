b8ff1125fe5a97b837326608e8b71500a31a6a9a
Richard <richardzou25@gmail.com>
2024-10-05 13:17:16 -0400

    finished exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function (startInt, endInt) {
+    let array = [];
+    let sumOfArray = 0;
 
+    if (Number.isInteger(startInt) == false || Number.isInteger(endInt) == false) {
+        return 'ERROR';
+    } else if (startInt < 0 || endInt < 0) {
+        return 'ERROR';
+    } else if (startInt > endInt) {
+        [startInt, endInt] = [endInt, startInt]
+        for (i = startInt; i <= endInt; i++) {
+            array.push(i)
+            sumOfArray += i;
+        };
+        return sumOfArray;
+    } else {
+        for (i = startInt; i <= endInt; i++) {
+            array.push(i)
+            sumOfArray += i;
+        };
+        return sumOfArray;
+    };
 };
 
 // Do not edit below this line
