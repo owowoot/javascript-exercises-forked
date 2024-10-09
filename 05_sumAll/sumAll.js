2348caefacf6b696f45f5a5c35ac6f2127be0ff7
Richard <richardzou25@gmail.com>
2024-10-09 09:07:40 -0400

    Cleaned up sumAll.js code.
    Removed unneeded array declaration and changed if
    statement to a for loop.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,25 +1,34 @@
-const sumAll = function (startInt, endInt) {
-    let array = [];
-    let sumOfArray = 0;
+const sumAll = function (startInt, endInt) { // 1,3
+    // let array = [];
+    // let sumOfArray = 0;
+    let result = 0;
+    let minNum = Math.min(startInt, endInt)
+    let maxNum = Math.max(startInt, endInt)
 
     if (Number.isInteger(startInt) == false || Number.isInteger(endInt) == false) {
         return 'ERROR';
     } else if (startInt < 0 || endInt < 0) {
         return 'ERROR';
-    } else if (startInt > endInt) {
-        [startInt, endInt] = [endInt, startInt]
-        for (i = startInt; i <= endInt; i++) {
-            array.push(i)
-            sumOfArray += i;
-        };
-        return sumOfArray;
-    } else {
-        for (i = startInt; i <= endInt; i++) {
-            array.push(i)
-            sumOfArray += i;
-        };
-        return sumOfArray;
-    };
+    }
+    for (let i = minNum; i <= maxNum; i++) {
+        result += i
+    }
+    return result;
+
+    // } else if (startInt > endInt) {
+    //     [startInt, endInt] = [endInt, startInt]
+    //     for (i = startInt; i <= endInt; i++) {
+    //         array.push(i)
+    //         sumOfArray += i;
+    //     };
+    //     return sumOfArray;
+    // } else {
+    //     for (i = startInt; i <= endInt; i++) {
+    //         array.push(i)
+    //         sumOfArray += i;
+    //     };
+    //     return sumOfArray;
+    // };
 };
 
 // Do not edit below this line
